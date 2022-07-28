7db0ae4d555d46185660c3028dc31dd7fa0071de
Juzuhay <103389606+Juhai123@users.noreply.github.com>
2022-07-28 21:58:59 +0800

    Add files via upload
    Update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\LoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,7 +20,7 @@ Route::get('/', function () {
 Route::get('/pegawai', [EmployeeController::class, 'index'])->name('pegawai');
 Route::get('/tambahpegawai', [EmployeeController::class, 'tambahpegawai'])->name('tambahpegawai');
 Route::post('/insertdata', [EmployeeController::class, 'insertdata'])->name('insertdata');
-Route::get('/tampilkandata/{id}', [EmployeeController::class, 'tampilkandata'])->name('tampilkandata');
+Route::get('/editdata/{id}', [EmployeeController::class, 'editdata'])->name('editdata');
 Route::post('/updatedata/{id}', [EmployeeController::class, 'updatedata'])->name('updatedata');
 Route::get('/delete/{id}', [EmployeeController::class, 'delete'])->name('delete');
     
